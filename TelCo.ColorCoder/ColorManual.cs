e8b56e7a66edd9f6eeeb571c9c56873b8bbe470c
Avanik Gupta <39823023+Avanik1@users.noreply.github.com>
2021-07-13 15:25:12 +0530

    Update ColorManual.cs

--- TelCo.ColorCoder/ColorManual.cs
+++ TelCo.ColorCoder/ColorManual.cs
@@ -8,11 +8,11 @@ namespace TelCo.ColorCoder
 {
     class ColorManual
     {
-        public static printColorManual()
+        public static void printColorManual()
         {
             int num = 1;
-            for(int i=1;i<=5;i++){
-                for(int j=1;j<=5;j++){
+            for(int i=0;i<5;i++){
+                for(int j=0;j<5;j++){
                     Console.WriteLine("Pair Number is {0}, Major Color is {1},Minor Color is {2}\n",num, Program.colorMapMajor[i], Program.colorMapMinor[j]);
                     num++;
                 }
