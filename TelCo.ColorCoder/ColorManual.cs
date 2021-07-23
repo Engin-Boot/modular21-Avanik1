55c84efdc0ba1cb9f739d0a3421087770a209b29
Avanik Gupta <avanik.gupta@publicissapient.com>
2021-07-23 12:38:05 +0530

    Initial Commit

--- TelCo.ColorCoder/ColorManual.cs
+++ TelCo.ColorCoder/ColorManual.cs
@@ -11,9 +11,11 @@ namespace TelCo.ColorCoder
         public static void printColorManual()
         {
             int num = 1;
-            for(int i=0;i<5;i++){
-                for(int j=0;j<5;j++){
-                    Console.WriteLine("Pair Number is {0}, Major Color is {1},Minor Color is {2}\n",num, Program.colorMapMajor[i], Program.colorMapMinor[j]);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Pair Number is {0}, Major Color is {1},Minor Color is {2}\n", num, Program.colorMapMajor[i], Program.colorMapMinor[j]);
                     num++;
                 }
             }
